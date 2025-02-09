449d1039e2cd2823619480e8e442cd411a336fce
tomas-rezabek <git@tomasrezabek.cz>
2025-02-09 20:36:50 +0100

    countWords function

---
+++ main.py
@@ -0,0 +1,23 @@
+
+def main():
+  with open("books/frankenstein.txt") as b:
+    file_contents = b.read()
+    print(file_contents)
+    
+def countWords():
+  with open("books/frankenstein.txt") as b:
+    file_contents = b.read()
+    file_words = file_contents.split()
+    num_of_words = 0
+    for w in file_words:
+      num_of_words += 1
+    print(num_of_words)
+      
+
+if __name__ == '__main__':
+  main()
+  countWords()
+    
+  
+
+  
